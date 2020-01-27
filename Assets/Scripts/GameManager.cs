76b69edbdd98857408f69cc4bc779e3f3f55d5e5
Kenny  Frueh <kenfrueh@uat.edu>
2020-01-27 12:13:25 -0700

    Added Project to repo

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{   //Manager when game start
+    public static GameManager instance;
+    //Track score
+    public int score = 0;
+    //Track pause boolean
+    public bool IsPaused = false;
+    public void Awake()
+    {
+        if (instance = null)
+        {
+            instance = this; //Shows THIS of class in int variable
+        }
+        else
+        {
+            Destroy(this.gameObject); //Destroy Game manager attached to component
+            Debug.LogError("Error: Tried to make second Game Manager");
+        }
+
+    }
+}
